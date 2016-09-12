b1f600fe47a3c123f7e67773873130fb711fdba5
myroslav <myroslavlutsiv@gmail.com>
2016-09-12 16:39:18 +0300

    "[INITIAL COMMIT]"

---
+++ dev/js/main.js
@@ -0,0 +1,16 @@
+
+// header
+@import 'main/header/header.js';
+
+// elements
+@import 'main/contentElements/slider.js';
+@import 'main/contentElements/carousel.js';
+@import 'main/contentElements/parallax.js';
+
+// plugins
+@import 'main/plugins/news/newsCarousel.js';
+@import 'main/plugins/news/news.js';
+
+@import 'main/suggest.js';
+
+@import 'main/general.js';
